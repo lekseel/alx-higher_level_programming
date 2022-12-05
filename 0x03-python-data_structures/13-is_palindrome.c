72fdb948c35d91fbb634e364dc7873768585084a
root <olalekanshogbaike@gmail.com>
2022-12-05 13:19:08 +0000

    turn around

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stddef.h>
 /**
  * reverse - reverses the second half of the list
  *
@@ -31,7 +31,7 @@ void reverse(listint_t **h_r)
  *
  * @h1: head of the first half
  * @h2: head of the second half
- * Return: 1 if are equals, 0 if not
+ * Return: 1 if equal, 0 otherwise
  */
 int compare(listint_t *h1, listint_t *h2)
 {
@@ -53,7 +53,6 @@ int compare(listint_t *h1, listint_t *h2)
 			return (0);
 		}
 	}
-
 	if (tmp1 == NULL && tmp2 == NULL)
 	{
 		return (1);
@@ -66,8 +65,8 @@ int compare(listint_t *h1, listint_t *h2)
  * is_palindrome - checks if a singly linked list
  * is a palindrome
  * @head: pointer to head of list
- * Return: 0 if it is not a palindrome,
- * 1 if it is a palndrome
+ * Return: 0 if not a palindrome,
+ * 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
@@ -109,6 +108,5 @@ int is_palindrome(listint_t **head)
 			prev_slow->next = scn_half;
 		}
 	}
-
 	return (isp);
 }
