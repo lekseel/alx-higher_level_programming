da53423943a5ec2b142136e6927cf0cc4ae47eb3
Lekseel <olalekanshogbaike@gmail.com>
2023-01-10 20:47:07 +0000

    Input out

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,42 +1,21 @@
 #!/usr/bin/python3
+""" Module that adds all arguments to a Python list, and then
+save them to a file
 """
-Module for log parsing scripts.
-"""
-
-
 import sys
+import json
 
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+filename = "add_item.json"
+with open(filename, 'a+') as f:
+    pass
 
-    def check_match(line):
-        '''Checks for regexp match in line.'''
-        try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except Exception as e:
-            pass
-
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
-    i = 1
-    try:
-        for line in sys.stdin:
-            check_match(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+try:
+    my_list = load_from_json_file(filename)
+except ValueError:
+    my_list = []
+list1 = sys.argv[1:]
+my_list += list1
+save_to_json_file(my_list, filename)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
+""" Module that adds all arguments to a Python list, and then
+save them to a file
 """
-    Python script that adds all args to a Python List.
-    List is then saved to a file.
-"""
-
-
 import sys
 import json
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-file = "add_item.json"
-json_list = []
+filename = "add_item.json"
+with open(filename, 'a+') as f:
+    pass
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
-
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
-save_to_json_file(json_list, file)
+try:
+    my_list = load_from_json_file(filename)
+except ValueError:
+    my_list = []
+list1 = sys.argv[1:]
+my_list += list1
+save_to_json_file(my_list, filename)
