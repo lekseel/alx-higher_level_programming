8d1e9436ef1d5d6665f0dd72eb8286fd9578d95a
lekseel <olalekanshogbaike@gmail.com>
2023-03-13 12:15:59 +0000

    jav it

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2], 10);
-if (isNaN(num)) {
+if (isNaN(Number(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + num);
+  console.log('My number: ' + Number(process.argv[2]));
 }
