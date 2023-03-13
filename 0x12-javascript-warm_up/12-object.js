3b4aaa69d32d021bf5f2603979e4978abed2c238
lekseel <olalekanshogbaike@gmail.com>
2023-03-13 21:17:50 +0000

    js

--- 0x12-javascript-warm_up/12-object.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const myObject = {
-  type: 'Object',
-  value: 12
-};
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+
+const myVar = parseInt(process.argv[2]);
+if (process.argv[2] === undefined || isNaN(myVar)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myVar);
+}
