e3ddb774452ae311255588fb2ea6b08d84035fd9
lekseel <olalekanshogbaike@gmail.com>
2023-03-13 17:02:31 +0000

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,11 @@
-#!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
