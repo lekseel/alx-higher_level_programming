a0c3395dc1a6b8bae060357b5d92cecb807fc3ef
lekseel <olalekanshogbaike@gmail.com>
2023-03-13 12:07:23 +0000

    javascript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,2 @@
-#!/usr/bin/node                                                          const arg1 = process.argv[2];                                            const arg2 = process.argv[3];                                            console.log(`${arg1} is ${arg2}`); 
+#!/usr/bin/node
+console.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node                                                          const arg = process.argv[2];                                             const num = parseInt(arg);                                               if (isNaN(num)) {                                                          console.log("Not a number");                                           } else {                                                                   console.log(`My number: ${num}`);                                      }
+#!/usr/bin/node
+const num = parseInt(process.argv[2], 10);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
+}
