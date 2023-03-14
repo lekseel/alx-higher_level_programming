f3548647834dd6dccec2ba33373354c14bb4d25d
lekseel <olalekanshogbaike@gmail.com>
2023-03-14 17:06:59 +0000

    good news

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
