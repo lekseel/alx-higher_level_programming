13b604853c6cca26410450983445132718f23b13
lekseel <olalekanshogbaike@gmail.com>
2023-03-14 17:00:56 +0000

    good news

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // If w or h is not a positive integer, create an empty object
-      Object.create(null);
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-}
-
-module.exports = Rectangle;
+};
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,43 +1,18 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // create an empty object if w or h is invalid
-      return;
-    }
-    this.width = w;
-    this.height = h;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    if (!this.width || !this.height) {
-      return;
-    }
-    let row = '';
-    for (let i = 0; i < this.width; i++) {
-      row += 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
-  rotate() {
-    if (!this.width || !this.height) {
-      return;
-    }
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
-    if (!this.width || !this.height) {
-      return;
-    }
-    this.width *= 2;
-    this.height *= 2;
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
-}
-
-module.exports = Rectangle;
+};
