74f471b3551db0f4ef5dab2d3dbb2581a2fb9038
lekseel <olalekanshogbaike@gmail.com>
2023-03-17 20:24:29 +0000

    mysql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa where
+name matches the argument passed to the script
+"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(state_name))
-    rows = cursor.fetchall()
+def filter_states():
+    """
+    Function that filters and displays the states that match the user input
+    """
+    # Connect to MySQL database
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+
+    # Create cursor to execute queries
+    cur = conn.cursor()
+
+    # Execute SQL query with user input and sort results by states.id
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
+
+    # Fetch all rows and print results
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
-    db.close()
+
+    # Close cursor and database connection
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    filter_states()
