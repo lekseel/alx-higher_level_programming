d40d4376bda1c465f18c58da00401b54b2ac97dc
lekseel <olalekanshogbaike@gmail.com>
2023-03-18 09:14:46 +0000

    relatioaln mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
+"""
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    # Take arguments from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host='localhost', port=3306, user=username,
-                         passwd=password, db=dbname)
-
-    # Create a cursor object
-    cur = db.cursor()
-
-    # Execute query to select all states starting with 'N'
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the rows
-    rows = cur.fetchall()
-
-    # Print the results
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa where
-name matches the argument passed to the script
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
-import MySQLdb
 import sys
-
-
-def filter_states():
-    """
-    Function that filters and displays the states that match the user input
-    """
-    # Connect to MySQL database
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2],
-                           db=sys.argv[3], charset="utf8")
-
-    # Create cursor to execute queries
-    cur = conn.cursor()
-
-    # Execute SQL query with user input and sort results by states.id
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                .format(sys.argv[4]))
-
-    # Fetch all rows and print results
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
-    cur.close()
-    conn.close()
-
+import MySQLdb
 
 if __name__ == "__main__":
-    filter_states()
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    states = c.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    c.close()
+    db.close()
